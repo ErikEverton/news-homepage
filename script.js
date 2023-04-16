6fd8e53a4fadd32da299d9725029691a0e88f101
ErikEverton <122756143+ErikEverton@users.noreply.github.com>
2023-04-15 22:26:38 -0300

    removendo testes

--- script.js
+++ script.js
@@ -6,16 +6,12 @@ const menu = document.querySelector(".menu-mobile");
 openMenu.addEventListener("click", openMenuFunction);
 closeMenu.addEventListener("click", closeMenuFunction);
 
-let menuOpen = false;
-
 function openMenuFunction() {
     menu.classList.add("enable");
     document.body.style.overflow = "hidden";
-    menuOpen = true;
 }
 
 function closeMenuFunction() {
     menu.classList.remove("enable");
     document.body.style.overflow = "auto";
-    menuOpen = false;
 }
